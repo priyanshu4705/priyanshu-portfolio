d98d720e93355af84c2a38b323c42fc9028b4e6e
priyanshu4705 <priyanshusrivastava4705@gmail.com>
2021-05-05 11:34:10 +0530

    install requirements

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,8 @@
-import styled from 'styled-components'
-
-const H = styled.h1`
-    font-size: 5rem;
-    font-weight: 500;
-    font-family: monospace;
-    text-align: center;
-`
-
 const Footer = () => {
     return (
-        <div>
-            this is a footer
-            <H>Coming Soon...</H>
-        </div>
+        <>
+            <p>this is a footer</p>
+        </>
     )
 }
 
